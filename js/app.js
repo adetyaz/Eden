119b8c6e6ad835c86ee75d63699b23cae66337ed
adetyaz <tayo.lasisi@yahoo.com>
2020-04-23 05:02:44 +0100

    finalised media queries and animations

--- js/app.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    let menuToggle = document.querySelector('.toggle');
-    let mobileNav = document.querySelector('.mobile-nav');
-    menuToggle.addEventListener('click', () => {
-        menuToggle.classList.toggle('animate');
-        mobileNav.classList.toggle('slide');
-    });
-})();
